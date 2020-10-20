d95673aed51ecb89b4472fe03fc8322f86823aec
Alexander Vaneyev <vaneyev@mail.ru>
2020-10-20 10:20:00 +0300

    Fix JdbcMealRepository

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepository.java
@@ -6,6 +6,7 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
@@ -22,8 +23,15 @@ public class JdbcMealRepository implements MealRepository {
 
     private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
+    private final SimpleJdbcInsert insertMeal;
+
     @Autowired
     public JdbcMealRepository(JdbcTemplate jdbcTemplate, NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.insertMeal = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id")
+                .usingColumns("user_id", "datetime", "description", "calories");
+
         this.jdbcTemplate = jdbcTemplate;
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
@@ -38,12 +46,8 @@ public class JdbcMealRepository implements MealRepository {
                 .addValue("calories", meal.getCalories());
 
         if (meal.isNew()) {
-            if (namedParameterJdbcTemplate.update(
-                    "INSERT INTO meals (user_id, datetime, description, calories) " +
-                            "VALUES (:userId, :dateTime, :description, :calories)",
-                    map) == 0) {
-                return null;
-            }
+            Number newKey = insertMeal.executeAndReturnKey(map);
+            meal.setId(newKey.intValue());
         } else if (namedParameterJdbcTemplate.update(
                 "UPDATE meals SET datetime=:dateTime, description=:description, calories=:calories " +
                         "WHERE id=:id AND user_id=:userId", map) == 0) {
@@ -74,8 +78,8 @@ public class JdbcMealRepository implements MealRepository {
                 .addValue("userId", userId)
                 .addValue("start", startDateTime)
                 .addValue("end", endDateTime);
-        return namedParameterJdbcTemplate.query("SELECT * FROM meals WHERE user_id=:userId AND " +
-                "(:start IS NULL OR datetime >= :start) AND (:end IS NULL OR datetime < :end) " +
+        return namedParameterJdbcTemplate.query("SELECT * FROM meals " +
+                "WHERE user_id=:userId AND datetime >= :start AND datetime < :end " +
                 "ORDER BY datetime DESC", map, ROW_MAPPER);
     }
 }
